76c00de72ba1b7457fb36f34d127ef1e93158c54
kamelkenich174-netizen <kamelkenich174@gmail.com>
2026-07-08 22:07:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/Transport-Fornaka/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
